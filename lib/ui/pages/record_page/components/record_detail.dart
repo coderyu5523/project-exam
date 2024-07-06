ba2baeaaa1763013cf9cdba98fa43dfbd7d8a645
coderyu5523 <coderyu5523@naver.com>
2024-07-06 20:15:08 +0900

    버튼 만들기

--- lib/ui/pages/record_page/components/record_detail.dart
+++ lib/ui/pages/record_page/components/record_detail.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_account/ui/_common/under_line_widget.dart';
 import 'package:project_account/ui/pages/record_page/components/record_list.dart';
-
 import '../../../../data/record/dummy.dart';
 
 class RecordDetail extends StatelessWidget {
@@ -9,54 +8,61 @@ class RecordDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return <Widget>[];
-      },
-      body: ListView.builder(
-        itemCount: recordList.length,
-        itemBuilder: (BuildContext context, int index) {
-          final record = recordList[index];
-          return Column(
-            children: [
-              Container(
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "${record['date']}일",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text("${record['income']}원", style: TextStyle(
-                              color: Colors.blue)),
-                          Text(
-                            "${record['outgo']}원",
-                            style: TextStyle(color: Colors.red),
-                          ),
-                        ],
-                      ),
-                    ),
-                    UnderLineWidget(),
-                    RecordList(items: record['records']),
-                    SizedBox(
-                      height: 8,
-                      child: Container(
-                        color: Colors.grey[200],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          );
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+        shape: CircleBorder(),
+      ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[];
         },
+        body: ListView.builder(
+          itemCount: recordList.length,
+          itemBuilder: (BuildContext context, int index) {
+            final record = recordList[index];
+            return Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "${record['date']}일",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text("${record['income']}원", style: TextStyle(color: Colors.blue)),
+                            Text(
+                              "${record['outgo']}원",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ],
+                        ),
+                      ),
+                      UnderLineWidget(),
+                      RecordList(items: record['records']),
+                      SizedBox(
+                        height: 8,
+                        child: Container(
+                          color: Colors.grey[200],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
